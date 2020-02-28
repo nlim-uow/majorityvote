4821091f76fb1de29734a1d688d50f35d8fcad92
nlim-uow <61566270+nlim-uow@users.noreply.github.com>
2020-02-29 00:12:40 +1300

    Add files via upload

--- GISETTE/gisette.m
+++ GISETTE/gisette.m
@@ -5,6 +5,21 @@ runCount=100;
 ensSize=100;
 %% Set Base classifiers 1=LDA-RS, 2=LDA-RP, 3=SVM-RS, 4=SVM-RP, 5=RF
 baseClassifier=5;
+for baseClassifier=1:5
+prefix='gisette';
+if baseClassifier==1
+    suffix='lda_rs';
+elseif baseClassifier==2
+    suffix='lda_rp';
+elseif baseClassifier==3
+    suffix='svm_rs';
+elseif baseClassifier==4
+    suffix='svm_rp';
+else
+    suffix='rf';
+end
+filespec=sprintf('%s_%s',prefix,suffix);
+
 
 RSSens=zeros(runCount,kCount,ensSize);
 RSSens2=zeros(runCount,kCount,ensSize);
@@ -194,7 +209,7 @@ close gcf;
       
 %% Plot the Empirical Accuracy vs PE-Model                
         
-sTitle=sprintf('Ensemble Accuracy vs Ensemble Size for GISETTE');
+sTitle=sprintf('Ensemble Accuracy vs Ensemble Size for %s',upper(prefix));
 [fig myAxes]=createAxes([2 2],'title',sTitle,'hSize',7.5,'vSize',8.5,'leftOffset',0,'legendHeight',0.8,'vMargin',1.25);
 myPlots=gobjects(1,6);
 plotNo=0;
@@ -249,9 +264,9 @@ plotNo=0;
     legendCell={'Empirical Majority Vote','Polya Model (Vote Correlation)','Polya Model (Jaccard Similarity)','Polya Model (Yule Diversity)','Binomial Model (Uncorrelated)','Polya Model (Sneath Diversity)'};
     legend(myPlots,legendCell,'Location',[0.4 0.045 0.3 0.04]);
     drawnow;
-    fileName=sprintf('polyaModel_gisette.fig');
+    fileName=sprintf('polyaModel_%s.fig',filespec);
     savefig(fileName);
-    fileName=sprintf('polyaModel_gisette.eps');
+    fileName=sprintf('polyaModel_%s.eps',filespec);
     saveas(gcf,fileName,'epsc');
 
     
@@ -273,7 +288,7 @@ rssRec;
 
 %% Plot the mean and 95-5% bootstrap empirical accuracy vs PE-Model
 
-sTitle=sprintf('Ensemble Accuracy vs Ensemble Size for GISETTE');
+sTitle=sprintf('Ensemble Accuracy vs Ensemble Size for %s',upper(prefix));
 [fig myAxes]=createAxes([2 2],'title',sTitle,'hSize',7.5,'vSize',8.5,'leftOffset',0,'legendHeight',0.8,'vMargin',1.25);
 myPlots=gobjects(1,4);
 plotNo=0;
@@ -314,12 +329,13 @@ plotNo=0;
     legendCell={'Empirical Majority Vote','Upper 95% Accuracy','Lower 5% Accuracy','Polya Model (Sneath Diversity)'};
     legend(myPlots,legendCell,'Location',[0.4 0.045 0.3 0.04]);
     drawnow;
-    fileName=sprintf('ci_gisette.fig');
+    fileName=sprintf('ci_%s.fig',filespec);
     savefig(fileName);
-    fileName=sprintf('ci_gisette.eps');
+    fileName=sprintf('ci_%s.eps',filespec);
     saveas(gcf,fileName,'epsc');
 
     
     
-  save gisette.mat  
+  save(filespec)
     
+end
